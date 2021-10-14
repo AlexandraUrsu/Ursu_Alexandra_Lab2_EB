4f7abdfebef9a7078e0d6cdb0dd13eed7d1bfd18
ALEXANDRA-ELENA URSU <alexandra.elena.ursu@stud.ubbcluj.ro>
2021-10-14 19:07:54 +0300

    Add project files.

---
+++ Ursu_Alexandra_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ursu_Alexandra_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
